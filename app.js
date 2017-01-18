6d2ca2daae8875ba0a117e2765d3b878048fa071
Erik Noonan <noonan135@gmail.com>
2017-01-18 14:05:19 -0600

    unbroken end product

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 //app.js
 var http=require('http');
 var modThree = require('./modThree.js');
-var result = modThree.modThree_0(a.toString().trim());
+var result = modThree.modThree_0(100, 1000000);
 
 http.createServer(function(req,res){
   res.writeHead(200);
 
+  res.write(modThree.modThree_1(result));
 
-  var stdin = process.openStdin();
-
-  stdin.addListener("data", function(d) {;
-  	var result = modThree.modThree_0(d.toString().trim());
-  	console.log(modThree.modThree_1(result));
-})
 
   res.end();
 }).listen(5000);
--- modThree.js
+++ modThree.js
@@ -3,7 +3,7 @@ var modOne = require('./modOne');
 var modTwo = require('./modTwo');
 
 exports.modThree_0 = function(number) {
-	return modTwo.modTwo(modOne.modOne(number, number*100));
+	return modTwo.modTwo(modOne.modOne(100, 1000000));
 }
 
 exports.modThree_1 = function(number) {
